69912504292d59954bffe315f40832e247470403
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-05 14:49:23 +0530

    add loop

---
+++ control_flow/loop.py
@@ -0,0 +1,20 @@
+my_list = ["sachin","dhoni","kohli","rohit"]
+coun_variable = 0
+while coun_variable<5:
+    print(coun_variable)
+    coun_variable+=1
+print("out of while loop")
+
+coun_variable = 0
+while coun_variable<10:
+    print("statement ",coun_variable)
+    coun_variable +=2
+
+
+for player in my_list:
+    print(player)
+
+for char in "INDIA":
+    print(char)
+
+print("At the End of the program!")
