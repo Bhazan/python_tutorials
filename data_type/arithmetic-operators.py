d81aeda4fec7f39229090f50dc48852ac24ac8f8
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-05 10:12:44 +0530

    add few more operator

--- data_type/arithmetic-operators.py
+++ data_type/arithmetic-operators.py
@@ -1,4 +1,15 @@
-print("Addition 3+3 = ",3+3)
-print("Subtraction 3-1 = ",3-1)
-print("Multiplication 5*3 = ",5*3)
-print("Division 5/2 = ",5/2)
+# Arithmetic operators
+
+print("Addition 3+3 = " ,3+3)
+
+print("Subtraction 3-1 = " ,3-1)
+
+print("Multiplication 5*3 = " ,5*3)
+
+print("Division 5/2 = ", 5/2)
+
+print("Modulus 5%2 = ", 5%2)
+
+print("Floor division 9//2 = ", 9//2)
+
+print("Exponential 2**4 = ", 2**4)
