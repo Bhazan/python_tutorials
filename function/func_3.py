6663a7cf049f73b6cc50ec83e9a7d1af513cd334
bhajandham <bhajandhami314@gmail.com>
2022-07-23 17:52:49 +0530

    default parameter

---
+++ function/func_3.py
@@ -0,0 +1,15 @@
+# A function with multiple arguments
+def my_function(arg1, arg2, arg3):
+    print(arg1, arg2, arg3)
+
+my_function(arg3 = "Bhajan", arg1 = "Sachin", arg2 = "Mahendra") # calling function with key value order doesn't matter
+
+# default parameter value
+
+def my_func(country = "India"):
+    print(country)
+
+my_func("USA")
+my_func("England")
+my_func()
+my_func("Austrailia")
