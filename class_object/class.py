d3c469eb6c699952e4c7a6e46f77d819ddb0ae25
bhajandham <bhajandhami314@gmail.com>
2022-07-23 16:07:31 +0530

    add class

---
+++ class_object/class.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+
+object_1 = Person("sachin", 23)
+print(object_1.name)
+print(object_1.age)
