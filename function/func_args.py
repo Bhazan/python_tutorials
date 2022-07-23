e89cb046de698fe273d3c25968ae6c4ed51f3bd8
bhajandham <bhajandhami314@gmail.com>
2022-07-23 17:46:07 +0530

    fucntion with args

---
+++ function/func_args.py
@@ -0,0 +1,7 @@
+# A function definition with arguments
+def my_function(fname):
+    print(fname+ " Singh")
+
+my_function("Bhajan") # calling a function with parameter
+my_function("Sachin")
+my_function("Virat")
