f705f9b9c836c170e47ed19b04c97f779770d6ea
bhajandham <bhajandhami314@gmail.com>
2022-07-23 17:40:03 +0530

    update file

---
+++ function/basic.py
@@ -0,0 +1,8 @@
+# In Python a function is defined using the def keyword
+
+def funcName():
+    print("Hello, Function!") # In Python a fucntion block is started with indentation like curly bracket in another java programming
+
+# Calling a function
+funcName()
+
