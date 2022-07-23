fec73e295aa5b7f3c3459c1e4b700c8cf31f0bba
bhajandham <bhajandhami314@gmail.com>
2022-07-23 16:12:35 +0530

    add another class

--- class_object/class.py
+++ class_object/class.py
@@ -1,3 +1,4 @@
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -7,3 +8,15 @@ class Person:
 object_1 = Person("sachin", 23)
 print(object_1.name)
 print(object_1.age)
+
+class A:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def fullName(self):
+        print("My Name is "+self.fname+" "+self.lname)
+    
+
+object_2 = A("Bhajan", "Singh")
+object_2.fullName()
